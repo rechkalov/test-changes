0f9ee07dc6e95fe0bd97eaa52646f94b60556bba
a.rechkalov <a.rechkalov@2gis.ru>
2017-03-13 18:04:22 +0700

    initial

---
+++ Testable/Testable.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testable.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class CalcTests
+    {
+        [NUnit.Framework.Test]
+        public void Add()
+        {
+            var calc = new Calc();
+            var sum = calc.Add(1, 2);
+            NUnit.Framework.Assert.AreEqual(3, sum);
+        }
+    }
+}
